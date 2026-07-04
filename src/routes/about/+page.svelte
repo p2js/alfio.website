678825b3b04df22aeb6bbffdcf409c68d70f8778
Alfio <tomarchioalfio5@gmail.com>
2026-07-04 22:51:28 +0100

    write more for about me

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -19,12 +19,16 @@
     <li>
         <b>The web and its technologies:</b> The web is incredible, and programming
         with web technologies has been my favourite part of the hobby since I started.
-        Making good web apps is hard, and is subject to a fairly unique set of constraints.
+        Making good web apps is hard and subject to a fairly unique set of constraints.
         This has resulted in tons of captivating conversations, philosophies and
         developments.
     </li>
-    <li><b>Programming languages and compilers:</b></li>
-    <li><b>Topology:</b> The subject fascinates me</li>
+    <li>
+        <b>Programming languages and compilers:</b>
+        I have a lot of fun exploring code optimization and rewriting, and the implications
+        of design decisions in different programming languages; I also love writing
+        my own parsers and compilers.
+    </li>
     <li>
         <b>Cars and Racing:</b> I've been an avid automotive enthusiast my whole
         life. I especially like small cars, weird cars and weird small cars. I also
@@ -34,8 +38,11 @@
         <b>Music:</b> I really enjoy listening to and analyzing music. My favourite
         genres are ambient, jazz and experimental.
     </li>
-    <li><b>Fonts:</b></li>
-    <li><b>Urbanism and City Design:</b></li>
+    <li>
+        <b>Urbanism and City Design:</b> While I am by no means an expert, I like
+        exploring the nuances of the intent and quirks in city design (particularly
+        zoning, aesthetics and road/transit networks).
+    </li>
 </ul>
 
 <p>
