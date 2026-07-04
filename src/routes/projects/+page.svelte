a8a0f2aa5f9e0211e3150a576dee3710b46eced2
Alfio <tomarchioalfio5@gmail.com>
2026-07-04 23:41:53 +0100

    add filtering for projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,13 +1,43 @@
 <script>
     import Project from "./Project.svelte";
     import { projects } from "./projects";
+
+    const categories = new Set(projects.map((p) => p.category));
+    const languages = new Set(projects.map((p) => p.language));
+
+    let showCategory = $state("");
+    let showLanguage = $state("");
+
+    let showProjects = $derived(
+        projects.filter(
+            (project) =>
+                (!showCategory || project.category === showCategory) &&
+                (!showLanguage || project.language === showLanguage),
+        ),
+    );
 </script>
 
 <h1>Projects</h1>
 
-<p>Filter by type:</p>
-<p>Or by language:</p>
+<p>
+    Filter by category:
+    <button onclick={() => (showCategory = "")}>
+        All
+    </button>{#each categories as category},
+        <button onclick={() => (showCategory = category)}>{category}</button>
+    {/each}
+</p>
+<p>
+    Or by language:
+    <button onclick={() => (showLanguage = "")}>
+        All
+    </button>{#each languages as language},
+        <button onclick={() => (showLanguage = language)}>{language}</button>
+    {/each}
+</p>
 
-{#each projects as project}
+<hr />
+
+{#each showProjects as project}
     <Project {...project} />
 {/each}
--- src/routes/projects/projects.js
+++ src/routes/projects/projects.js
@@ -10,22 +10,36 @@
 export const projects = [
     {
         name: "Xenon",
+        repo: "p2js/xenon",
         description: `
-        A lightweight implementatino of a component system for static HTML,
+        A lightweight implementation of a component system for static HTML,
         using less than 650 bytes of JavaScript.
         Its primary use was to display my projects on the old version
         of my website.
         `,
-        category: "library",
+        category: "Library",
         language: "HTML",
         snippet: `<template _="greeting" name>
     <p>Hello, {name}!</p>
 </template>
-<greeting name="world"></greeting>
+<greeting name="World"></greeting>
 <!-- <p>Hello, World!</p> -->`,
     },
     {
         name: "RSFormat",
-        description: "Minimal yet powerful JavaScript formatting and printing library.",
+        repo: "p2js/rsformat",
+        description: `
+            A minimal, but powerful JavaScript formatting and printing library.
+            It's inspired by Rust's formatting helpers, hence the name.
+        `,
+        category: "Library",
+        language: "Javascript",
+        snippet: `import { rs, println } from "rsformat";
+
+let str = rs\`\${15} is \${rs.ref(0)}:X in hex\`;
+// s == "15 is 0xF in hex"
+
+println(rs\`\${'a'}:>7\`);
+// prints "      a"`,
     },
 ];
